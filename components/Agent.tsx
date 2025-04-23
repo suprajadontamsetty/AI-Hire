04b83df64d6ab5c39e7f8baac91274925e04b43a
Supraja <suprajadb@gmail.com>
2025-04-24 00:10:55 +0530

    show feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation';
 import { useState,useEffect } from 'react';
 import { vapi } from '@/lib/vapi.sdk';
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 
 
 
@@ -64,10 +65,11 @@ const Agent = ({userName,userId,type,interviewId, questions}:AgentProps) => {
     const handleGenerateFeedback=async(messages:SavedMessage[])=>{
         console.log('Generate feedbck here.');
 
-        const {success, id} = {
-            success:true,
-            id:'feedback-id'
-        }
+        const {success, feedbackId: id} = await createFeedback({
+            interviewId:interviewId!,
+            userId:userId!,
+            transcript:messages
+        })
 
         if(success && id) {
             router.push(`/interview/${interviewId}/feedback`);
