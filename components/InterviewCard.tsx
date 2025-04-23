a70699aee50b7f776c6c23af9ddc6f43ee4b5b45
Supraja <suprajadb@gmail.com>
2025-04-23 22:49:59 +0530

    implement interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
 
 
-const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
+const InterviewCard = ({id, userId, role, type, techstack, createdAt}: InterviewCardProps) => {
     const feedback=null as Feedback|null;
     const normalizedType=/mix/gi.test(type)?'Mixed':type;
     const formattedDate=dayjs(feedback?.createdAt || createdAt|| Date.now()).format('MMM D, YYYY');
@@ -52,8 +52,8 @@ const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt}: 
                 <DisplayTechIcons techStack={techstack}/>
                 <Button className='btn-primary'>
                     <Link href={feedback
-                      ? `/interview/${interviewId}/feedback`
-                      : `/interview/${interviewId}`
+                      ? `/interview/${id}/feedback`
+                      : `/interview/${id}`
 
                     }>
                         {feedback?'Check Feedback':'View Interview'}
